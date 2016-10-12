fc2f9e59c2b508d183091360f632945c1fffe0c7
Ajay Todimala <ajay.todimala@gmail.com>
2016-10-12 17:24:08 -0500

    Initial Commit

---
+++ HelloWorldSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldSwift
+//
+//  Created by Manasa Velpula on 10/12/16.
+//  Copyright © 2016 Todimala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
